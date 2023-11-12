960092f3d534b34f0202aa2cdec8f87e4d2372d9
Nbelman1 <nbelman1@gmail.com>
2023-11-12 11:28:30 -0500

    Updated playRound function. Reordered functions. Tested playGame function.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 
 let randomInt = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 
-
 function getComputerChoice() {
     if (randomInt === 1) {
         return 'rock';
@@ -14,66 +13,81 @@ function getComputerChoice() {
     } else if (randomInt === 3) {
         return 'scissors';
     } else {
-        return 'error: not recognized';
+        return 'error: game is broken';
     }
 } 
 
-console.log('Computer\'s choice: ' + getComputerChoice());
 let computerSelection = getComputerChoice();
 
+//function getPlayerChoice
+//prompt player for their choice
+//make input case-insensitive
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose your weapon: rock, paper, or scissors?", "Enter choice here");
+    return playerChoice.toLowerCase(); 
+}
+
+let playerSelection = getPlayerChoice();
 
 //function playRound
 //take two parameters, playerSelection and computerSelection
-    //make choice case-insensitive
 //weigh two choices together
 //return string stating result
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
-        return 'Tie game! No points this round.';
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        return 'Rock beats scissors! One point for humans.';
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        return 'Paper beats rock! One point for humans.';
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        return 'Scissors beats paper! One point for humans.';
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        return 'Paper beats rock! One point for the machines.'; 
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        return 'Scissors beats paper! One point for the machines.';
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-        return 'Rock beats scissors! One point for the machines.';
+    ++roundNumber;
+    if (playerSelection === computerSelection) {
+        return console.log('Tie game! No points this round.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        ++playerScore;
+        return console.log('Rock beats scissors! One point for humans.');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        ++playerScore;
+        return console.log('Paper beats rock! One point for humans.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        ++playerScore;
+        return console.log('Scissors beats paper! One point for humans.');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        ++computerScore;
+        return console.log('Paper beats rock! One point for the machines.'); 
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        ++computerScore;
+        return console.log('Scissors beats paper! One point for the machines.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        ++computerScore;
+        return console.log('Rock beats scissors! One point for the machines.');
     }
 }
 
-//function game
+console.log('Player\'s choice: ' + playerSelection);
+console.log('Computer\'s choice: ' + computerSelection);
+
+//function playGame
 //run playRound function 5 times
 //keep score
 //declare winner with string
-let playerScore = 0;
-let computerScore = 0;
-let playerSelection = prompt("Choose your weapon: rock, paper, or scissors?", "Enter choice here");
 
-console.log('Player\'s choice: ' + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
-function assignPoint() {
-    if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') ||
-        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')) {
-            return ++playerScore;
-    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') ||
-            (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') || 
-            (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock')) {
-            return ++computerScore;
+function playGame() {
+    if (roundNumber < 5) {
+        playRound();
+    } else if (roundNumber > 5) {
+        if (playerScore > computerScore) {
+            return `Congrats, you\'ve saved the humans! Player won with ${playerScore} points.`;
+        } else if (computerScore > playerScore) {
+            return `Oh no, computers have taken over! Computer won with ${computerScore} points.`;
+        } else {
+            return 'Tie game! Humans and computers live together peacefully.';
+        }
+    } else {
+        return 'error: game is broken.';
     }
 }
 
-function game() {
-    playRound(playerSelection, computerSelection);
-    assignPoint();
-}
-
-console.log('Human\'s points: ' + playerScore);
-console.log('Machine\'s points: ' + computerScore);
-
+console.log('Player\'s score: ' + playerScore);
+console.log('Computer\'s score: ' + computerScore);
+console.log('Round number: ' + roundNumber);
